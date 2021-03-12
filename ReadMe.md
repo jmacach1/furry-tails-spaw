fdf1c6823c5214d56a6e0ad596e61bd67fdf185c
jerald macachor <jerald.macachor@gmail.com>
2021-03-12 00:47:15 -0800

    outputing pets on html

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,8 @@
 # Pet's Salon Project
+
+Simulate a Pet Salon with Pet registration for a range of Pet grooming services.
+
+## Stack
+
+HTML 5, CSS 3, JavaScript ES2016
+BootStrap 4.3.1
--- js/main.js
+++ js/main.js
@@ -1,15 +1,78 @@
-class Pet {
-  constructor(name, age, gender, breed, service, owner, phone) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
-    this.breed = breed;
-    this.service= service;
-    this.owner = owner;
-    this.phone = phone;
-  }
+const salon = {
+  name: "Furry Tails Spaw",
+  address: {
+    city: "Irvine",
+    street: "235 Alton Parkway Suite 200"
+  },
+  hours: {
+    open: '10:00 am',
+    close: '5:00 pm'
+  },
+  pets: [
+    {
+      name: "Scooby",
+      age: 50,
+      gender: "Male",
+      breed: "Dane",
+      service: "Shower",
+      owner: "Shaggy",
+      phone: "555-555-5555"
+    },
+    {
+      name: "Scrapy",
+      age: 40,
+      gender: "Male",
+      breed: "Dane",
+      service: "Nails Cut",
+      owner: "Shaggy",
+      phone: "555-555-5555"
+    },
+    {
+      name: "Speedy",
+      age: 10,
+      gender: "Male",
+      breed: "Mixed",
+      service: "Full Service",
+      owner: "Bugs Bunny",
+      phone: "888-888-8888"
+    },
+    {
+      name: "Snoopy",
+      age: 35,
+      gender: "Male",
+      breed: "Beagle",
+      service: "Full Service",
+      owner: "Charlie Brown",
+      phone: "444-555-6666"
+    }
+  ]
 }
 
-const scooby = new Pet("Scooby", 50, "male", "Dane", "Full Service", "Shaggy", "555-555-5555");
-const scrappy = new Pet("Scrappy", 20, "male", "Dane", "Nails Cut", "Shaggy", "555-555-5555");
-const pets = [scooby, scrappy];
+const display = document.getElementById('display');
+const { pets } = salon;
+const names = [];
+
+for (let pet of pets) {
+  names.push(`<p>${pet.name}</p>`);
+}
+
+display.innerHTML = `
+  <p>Number of Pets: ${pets.length}</p>
+  ${names.join('\n')}
+`;
+
+// class Pet {
+//   constructor(name, age, gender, breed, service, owner, phone) {
+//     this.name = name;
+//     this.age = age;
+//     this.gender = gender;
+//     this.breed = breed;
+//     this.service= service;
+//     this.owner = owner;
+//     this.phone = phone;
+//   }
+// }
+
+// const scooby = new Pet("Scooby", 50, "male", "Dane", "Full Service", "Shaggy", "555-555-5555");
+// const scrappy = new Pet("Scrappy", 20, "male", "Dane", "Nails Cut", "Shaggy", "555-555-5555");
+// const pets = [scooby, scrappy];
