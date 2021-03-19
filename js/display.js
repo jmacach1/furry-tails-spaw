d4336058b128938fe3fe88009a42c58ae015ea65
jerald macachor <jerald.macachor@gmail.com>
2021-03-18 19:27:20 -0700

    change theme

--- js/display.js
+++ js/display.js
@@ -1,17 +1,3 @@
-// function randomBGcolor(transparency = 80) {
-//   const t = transparency / 100;
-//   const colors = [
-//     `rgba(0, 123, 255, ${t})`, // blue
-//     `rgba(255, 193, 7, ${t})`, // yellow
-//     `rgba(253, 126, 20, ${t})`, // orange
-//     `rgba(40, 167, 69, ${t})`, // green
-//     `rgba(110, 66, 193, ${t})`, // purple
-//     `rgba(220, 53, 70, ${t})` // red
-//   ];
-//   const index = Math.trunc(Math.random() * colors.length);
-//   return colors[index];
-// }
-
 const blue = `rgba(0, 123, 255, .5)`;
 const yellow = `rgba(255, 193, 7, .5)`;
 const green = `rgba(40, 167, 69, .5)`;
@@ -52,21 +38,21 @@ function createCard(pet) {
   const spc = getPriceColorByService(pet.service);
   pet.price = spc.price;
   return `
-  <div class="card" style="width: 15rem; background-color: ${spc.color};">
-    <div class="card-header">
-      <h5 class="card-title">${pet.name} ${pet.icon}</h5>
-      <h6 class="card-subtitle mb-2">Owned by: ${pet.owner}</h6>
-      <h6 class="card-subtitle mb-2">Phone: ${pet.phone}</h6>
+    <div class="card" style="width: 15rem; background-color: ${spc.color};">
+      <div class="card-header">
+        <h5 class="card-title">${pet.name} ${pet.icon}</h5>
+        <h6 class="card-subtitle mb-2">Owned by: ${pet.owner}</h6>
+        <h6 class="card-subtitle mb-2">Phone: ${pet.phone}</h6>
+      </div>
+      <div class="card-body">
+        <p class="card-text">Age: ${pet.age}</p>
+        <p class="card-text">Gender: ${pet.gender}</p>
+        <p class="card-text">Type: ${pet.petType}</p>
+        <p class="card-text">Breed: ${pet.breed}</p>
+        <p class="card-text">Service: ${pet.service}</p>
+        <p class="card-text">Price: $${pet.price}</p>
+      </div>
     </div>
-    <div class="card-body">
-      <p class="card-text">Age: ${pet.age}</p>
-      <p class="card-text">Gender: ${pet.gender}</p>
-      <p class="card-text">Type: ${pet.petType}</p>
-      <p class="card-text">Breed: ${pet.breed}</p>
-      <p class="card-text">Service: ${pet.service}</p>
-      <p class="card-text">Price: $${pet.price}</p>
-    </div>
-  </div>
   `;
 }
 
